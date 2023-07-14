e2e97673d62e39c8464a7def2a7b4eb2203493a0
Egor Miliukov <milukove@gmail.com>
2023-07-14 23:52:47 +0300

    fix cds and chains fetching issue

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -4,8 +4,8 @@ export const WEB2_CONNECTORS_PATH =
 export const WEB3_CONNECTORS_PATH =
   'https://api.github.com/repos/grindery-io/grindery-nexus-schema-v2/contents/cds/web3';
 
-export const CHAINS_PATH = 'https://cds.grindery.org/chains';
-export const CHAINS_STAGING_PATH = 'https://cds-staging.grindery.org/chains';
+export const CHAINS_PATH = 'https://cds.grindery.io/chains';
+export const CHAINS_STAGING_PATH = 'https://cds-staging.grindery.io/chains';
 
-export const DRIVERS_URL = 'https://cds.grindery.org';
-export const DRIVERS_STAGING_URL = 'https://cds-staging.grindery.org';
+export const DRIVERS_URL = 'https://cds.grindery.io';
+export const DRIVERS_STAGING_URL = 'https://cds-staging.grindery.io';
